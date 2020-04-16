6d5a31b0ba67d41fb8a59fcc0e38111a6f8d12df
TAKERU ARAKAWA <takeruarakawa@TAKERUnoMacBook-Air.local>
2020-04-16 20:03:07 +0900

    [Update]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,12 @@
+class BooksController < ApplicationController
+	def index
+	end
+	def edit
+	end
+	def show
+	end
+	def update
+	end
+	def deleate
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get '/' => 'homes#top'
+  get '/home/about' => 'homes#show'
+  get '/users/sign_out' => 'sessions#destroy'
+  resources :users, only:[:index,:show,:edit,:update]
+  resources :books,only:[:new,:create,:index,:show,:edit,:update,:delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
