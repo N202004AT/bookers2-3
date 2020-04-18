67386caafab900de026946edf7bfc165e8f2b28b
TAKERU ARAKAWA <takeruarakawa@TAKERUnoMacBook-Air.local>
2020-04-18 23:49:10 +0900

    [Update]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cc2b194212dcecebb9f3907d3f559432031620447d68e05b8dbd536d8fc69012b6b4367c8007046db6c9b059d7ca0ea399b78dadbf99ef74a2daa7ec8c774020'
